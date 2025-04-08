c567ce4db3d4016a657ce87cec472047f5253283
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:18:04 -0300

    Add project files.

---
+++ ListaExercicios/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lista_de_Exercicios.exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region imput de dados
+            decimal largura, altura, comprimento, resultado;
+            Console.WriteLine("Digite o comprimento da caixa");
+            comprimento = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a altura");
+            altura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a Largura");
+            largura = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            resultado = largura * altura * comprimento;
+
+            Console.WriteLine($"o volume da caixa é {resultado}");
+            Console.ReadLine();
+
+        }
+    }
+}
