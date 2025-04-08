bea09b516e7fac2884324a6173921b5047f0508a
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:21:56 -0300

    Consumo de combustível

---
+++ Exercício03/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal kmInicial, kmFinal, consumo, distancia, consumoKm;
+
+
+            Console.WriteLine("Infome o Km inicial");
+            kmInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Infome o Km final");
+            kmFinal = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("infome a quantidade combustivel abastecido");
+            consumo = Convert.ToDecimal(Console.ReadLine());
+
+            distancia = kmFinal - kmInicial;
+
+            consumoKm = distancia / consumo;
+            consumoKm = Math.Round(consumoKm, 2);
+
+            Console.WriteLine($"Foram gastos: {consumoKm} Litros de combustivel ");
+            Console.ReadLine();
+        }
+    }
+}
