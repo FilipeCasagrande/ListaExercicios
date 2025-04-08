9d21ed34819c3eab46fffb049bbb111b0149b3bb
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:28:56 -0300

    Números Primos

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            double raiz = 0;
+            Console.WriteLine("Informe o número para ver se ele é um número primo");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero <= 1)
+            {
+                Console.WriteLine($"O número {numero} não é um número primo");
+            }
+
+            for (int i = 2; i < numero; i++)
+            {
+                if (raiz % i != 0)
+                {
+
+                    Console.WriteLine("O número não é primo");
+                }
+
+                else
+                {
+                    Console.WriteLine($"O número é primo");
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
