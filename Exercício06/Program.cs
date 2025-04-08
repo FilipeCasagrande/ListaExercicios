435b97aa509d44fe12ffe1219fd63c7e1743a63e
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:27:55 -0300

    Notas e Pesos

---
+++ Exercício06/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal nota1, peso1, nota2, peso2, notaFinal, peso;
+
+            Console.Write("informe o valor da primeira nota: ");
+            nota1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o  Peso da primeira nota: ");
+            peso1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("informe o valor da segunda nota: ");
+            nota2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Informe o  Peso da segunda nota: ");
+            peso2 = Convert.ToDecimal(Console.ReadLine());
+
+            nota1 = nota1 * peso1;
+            nota2 = nota2 * peso2;
+            peso = peso1 + peso2;
+
+            notaFinal = (nota1 + nota2) / peso;
+
+            Console.WriteLine($"A nota final do Aluno(a) foi: {notaFinal}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
