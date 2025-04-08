0d3f8f17a5f720fa19006a2a448e250d256277a0
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:38:14 -0300

    Fatorial

---
+++ Exercício13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace exercicio13
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int contador, resultado = 1;
+            Console.WriteLine("Informe o valor de A para imprimir a sequência");
+            contador = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = contador; i >= 1; i--)
+            {
+                if (i != contador)
+                {
+                    Console.Write($" X ");
+                }
+                Console.Write(i);
+                resultado *= i;
+            }
+            Console.WriteLine($" = {resultado}");
+            Console.ReadLine();
+        }
+    }
+}
