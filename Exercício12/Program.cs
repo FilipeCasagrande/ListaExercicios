d5f99bf26969d523e88a50966d69573c176d983d
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:34:33 -0300

    Números 100 a 200

---
+++ Exercício12/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 101;
+
+            Console.WriteLine("Aperte ENTER para escrever os números impares de 100 a 200");
+            Console.ReadLine();
+
+            while (true)
+            {
+                if (numero < 200)
+                {
+                    Console.WriteLine("Número:" + numero);
+                    numero = numero + 2;
+                }
+
+                else if (numero >= 200)
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
