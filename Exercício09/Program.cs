d475e35560609fc1967c08938c29301b5362ee08
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:31:00 -0300

    Ordem decrescente

---
+++ Exercício09/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tam = 5;
+            int[] numero = new int[tam];
+            int cont = 0;
+
+            {
+                for (int i = 0; i < tam; i++)
+                {
+                    Console.WriteLine("Informe um numero");
+                    numero[cont] = Convert.ToInt32(Console.ReadLine());
+                    cont++;
+
+                    if (cont == tam)
+                    {
+                        break;
+                    }
+                }
+
+                Array.Sort(numero);
+                Array.Reverse(numero);
+
+                Console.WriteLine("Os numeros da lista em forma decrescente são:");
+
+                foreach (int numeros in numero)
+                {
+                    Console.WriteLine($"numero: {numeros}");
+                }
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
