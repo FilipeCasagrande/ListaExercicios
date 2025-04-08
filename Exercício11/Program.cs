def172094903640e3a8241a2d633384e47510859
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:33:17 -0300

    Par ou Impar

---
+++ Exercício11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numero;
+            Console.Write("informe um número para saber se ele é Par ou Impar: ");
+            numero = Convert.ToDecimal(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine($"o número {numero} é Par");
+            }
+            else if (numero % 2 != 0)
+            {
+                Console.WriteLine($"O número {numero} é Impar");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
