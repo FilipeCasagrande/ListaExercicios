03b865fe9854fabb916e56a6b428582ce35a8ba5
Filipe <Filipecasagrande1@gmail.com>
2025-04-07 23:25:29 -0300

    Conversão de Celsius

---
+++ Exercício04/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, f = 32, conversao;
+
+            Console.Write("informe a temperatura em Celsius para a convesão: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            conversao = c * 9 / 5 + 32;
+
+            Console.WriteLine($"O valor da temperatura na escala Fahrenheit é {conversao}ºF");
+            Console.ReadLine();
+        }
+    }
+}
